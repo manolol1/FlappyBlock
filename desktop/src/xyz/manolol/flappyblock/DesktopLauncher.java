bd9459082ee7489a4f36a46750bc457bb0e0d406
Mario Ebenhofer <manolol@mailbox.org>
2024-02-04 12:04:15 +0100

    Initial commit

---
+++ desktop/src/xyz/manolol/flappyblock/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package xyz.manolol.flappyblock;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import xyz.manolol.flappyblock.Main;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("FlappyBlock");
+		new Lwjgl3Application(new Main(), config);
+	}
+}
